128eb55e45a13a3eedb594862dc64310e28bc895
mounikagone5 <84224628+mounikagone5@users.noreply.github.com>
2021-10-25 07:02:35 +0530

    Add files via upload

---
+++ eventmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practical1.Models
+{
+   
+    public class eventmodel
+    {
+        public int eventcode { get; set; }
+        public string title { get; set; }
+        public string typeofevent { get; set; }
+        public int estimatecost { get; set; }
+        public int actualcost { get; set; }
+        public DateTime dateofevent { get; set; }
+    }
+}
